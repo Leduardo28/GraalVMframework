237867a68b5f05bd2c28950536ec0af3705ddc48
Leduardo28 <lalin20050@outlook.com>
2025-07-20 16:52:43 -0600

    Revert " Implementar traductor Java a Python, JS y C"

--- Hola.java
+++
@@ -1,6 +0,0 @@
-public class Hola {
-    public static void main(String[] args) {
-        int a = 10;
-        System.out.println("Hola desde Java");
-    }
-}
--- Translators/PyScripts/JavaToPython/HolaMundo.py
+++
@@ -1,7 +0,0 @@
-class HolaMundo:
-    def main():
-        numero = 5
-        print("Hola Mundo")
-        for i in range(0, 3):
-            print(i)
-if __name__ == "__main__": HolaMundo.main()
